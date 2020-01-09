ceea88be2f97796386f8446ba9f8ebe146466de8
João Victor <jvc.martins12@gmail.com>
2020-01-08 23:41:09 -0300

    Refatorando para utilizar prepared statement

--- src/TestaListagem.java
+++ src/TestaListagem.java
@@ -1,27 +1,28 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class TestaListagem {
 
 	public static void main(String[] args) throws SQLException {
-		Connection connection =
-				DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "root");
 
-		Statement stm = connection.createStatement();
-		boolean resultado = stm.execute("SELECT * FROM PRODUTO");
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection connection = connectionFactory.recuperarConexao();
 
+		PreparedStatement stm = connection.prepareStatement("SELECT ID, NOME, DESCRICAO FROM PRODUTO");
+		stm.execute();
 		ResultSet rst = stm.getResultSet();
 		while(rst.next()) {
-			int id = rst.getInt("id");
-			String nome = rst.getString("nome");
-			String descricao = rst.getString("descricao");
+			Integer id = rst.getInt("ID");
+			String nome = rst.getString("NOME");
+			String descricao = rst.getString("DESCRICAO");
 			System.out.println(id);
 			System.out.println(nome);
 			System.out.println(descricao);
 		}
+		rst.close();
+		stm.close();
+		connection.close();
 	}
-
 }
