8daa7787b935e2c01be60fc7cd23eb5e046448bb
João Victor <jvc.martins12@gmail.com>
2020-01-16 23:53:15 -0300

    Ajuste em pacotes

--- src/br/com/alura/jdbc/TestaInsercaoComProduto.java
+++ src/br/com/alura/jdbc/TestaInsercaoComProduto.java
@@ -3,6 +3,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import br.com.alura.jdbc.dao.ProdutoDAO;
+import br.com.alura.jdbc.factory.ConnectionFactory;
 import br.com.alura.jdbc.modelo.Produto;
 
 public class TestaInsercaoComProduto {
